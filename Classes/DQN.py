1c7ff73f95d18fdb03f61c35c1c4588232c72f6e
James Parkington <pongs.acreage.0o@icloud.com>
2024-06-26 22:27:15 -0400

    Add batch normalization and dueling architecture to DDQN

--- Classes/DQN.py
+++ Classes/DQN.py
@@ -6,21 +6,38 @@ class DQN(nn.Module):
         self.action_count     = action_count
         self.state_dimensions = state_dimensions  # Expect (height, width, channels)
 
-        self.network = nn.Sequential(
-            nn.Conv2d(state_dimensions[2], 32, kernel_size = 8, stride = 4),
+        self.feature_extraction = nn.Sequential(
+            nn.Conv2d(state_dimensions[2], 32, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size = 4, stride = 2),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size = 3, stride = 1),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU(),
-            nn.Flatten(),
+            nn.BatchNorm2d(64),
+            nn.Flatten()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.LazyLinear(512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
             nn.LazyLinear(512),
             nn.ReLU(),
             nn.Linear(512, action_count)
         )
 
     def forward(self, state):
-        return self.network(self.preprocess_state(state))
+        features   = self.feature_extraction(self.preprocess_state(state))
+        values     = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        
+        q_values = values + (advantages - advantages.mean(dim = 1, keepdim = True))
+        return q_values
 
     def preprocess_state(self, state):
         if state.dim() == 3:
